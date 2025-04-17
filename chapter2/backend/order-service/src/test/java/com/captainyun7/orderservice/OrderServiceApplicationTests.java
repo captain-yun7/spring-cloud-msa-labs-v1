218b1246d2e45e9c423c76b4c4b9e23b3d8d3308
captain-yun7 <jslovejs182@naver.com>
2025-04-17 17:30:20 +0900

    copy chapter1 to chatper2 folder

---
+++ chapter2/backend/order-service/src/test/java/com/captainyun7/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captainyun7.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ chapter2/backend/payment-service/src/test/java/com/captainyun7/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captainyun7.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
