9ab59d241755ff95cd06ed62c28365dbcc95fa93
yun\user <captain-yun7@github.com>
2025-04-22 17:43:08 +0900

    refactor : remove chapter2/ folder

--- chapter2/backend/product-service/src/main/java/com/captainyun7/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.captainyun7.product.repository;
-
-import com.captainyun7.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-} 
